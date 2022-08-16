6942ed597a0236aca62d44c908dee050d04bbde0
guilhermedom <guilhermedfs@gmail.com>
2022-08-15 23:04:43 -0300

    Change backend to make response texts more clear

--- app/server.R
+++ app/server.R
@@ -10,29 +10,31 @@ shinyServer(function(input, output) {
         startPredictionInterval = lambda - 2
         endPredictionInterval = lambda + 2
         
-        predictions = as.character(startPredictionInterval:endPredictionInterval)
+        predictions = startPredictionInterval:endPredictionInterval
         
         if (type == 1) {
             y = dpois(startPredictionInterval:endPredictionInterval, lambda = lambda)
-            z = as.character(round(y, 4))
-            typeTitle = "Probability of failing exactly x times"
-            labels = paste(predictions, "failures", "probability:", z)
+            z = round(y, 4)
+            typeTitle = paste("Probability of failing exactly", lambda, "times")
+            labels = paste("Exactly", predictions, "failures\n", "probability:", z * 100, "%")
         } 
         else if (type == 2) {
             y = ppois(startPredictionInterval:endPredictionInterval, lambda = lambda)
-            z = as.character(round(y, 4))
-            typeTitle = "Probability of failing less than x times"
-            labels = paste("<", predictions, "failures", "probability:", z)
+            z = round(y, 4)
+            typeTitle = paste("Probability of failing less than", lambda, "times")
+            labels = paste("Less than", predictions, "failures\n", "probability:", z * 100, "%")
         } 
         else if (type == 3) {
-            y = ppois(startPredictionInterval:endPredictionInterval, lambda = lambda, lower.tail = F)
-            z = as.character(round(y, 4))
-            typeTitle = "Probability of failing more than x times"
-            labels = paste(">", predictions, "failures", "probability:", z)
+            y = ppois(startPredictionInterval:endPredictionInterval, lambda = lambda,
+                      lower.tail = F)
+            z = round(y, 4)
+            typeTitle = paste("Probability of failing more than", lambda, "times")
+            labels = paste("More than", predictions, "failures\n", "probability:", z * 100, "%")
         }
         
         output$probabilityPlotID = renderPlot({
-            barplot(y, names.arg = labels, col = heat.colors(5, rev = T), ylim = c(0, 1), main = typeTitle)
+            barplot(y, names.arg = labels, col = heat.colors(5, rev = T),
+                    ylim = c(0, 1), main = typeTitle)
             box()
         })
     })
