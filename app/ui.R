7984cfc8e532988ca21c8641833e30857f85330e
guilhermedom <guilhermedfs@gmail.com>
2022-08-15 23:01:30 -0300

    Make style changes to UI

--- app/ui.R
+++ app/ui.R
@@ -6,17 +6,24 @@ shinyUI(fluidPage(
     theme = shinytheme("lumen"),
 
     # Application title
-    titlePanel(h1("Machine Failure Prediction Using Poisson Distribution", align = "center")),
+    titlePanel(h1("Machine Failure Prediction Using the Poisson Distribution",
+                  align = "center"),
+               windowTitle = "Machine Failure Prediction Using the Poisson Distribution"),
+    
     br(),
     br(),
 
     fluidRow(
-        column(6, align = "center", radioButtons("radioButtonID", label = "Select probability to predict:",
-               choices = list("Probability of failing exactly x times" = 1,
-                              "Probability of failing less than x times" = 2,
-                              "Probability of failing more than x times" = 3),
+        column(6, align = "center",
+               radioButtons("radioButtonID", label = "Select probability to predict:",
+               choices = list("Probability of machine failing exactly x times" = 1,
+                              "Probability of machine failing less than x times" = 2,
+                              "Probability of machine failing more than x times" = 3),
                selected = 1)),
-        column(6, align = "center", numericInput("numericInputID", "Average number of failures informed by the machine manufacturer:", value = 2, min = 1, max = 999),
+        column(6, align = "center",
+               numericInput("numericInputID", "Average number of failures
+                            informed by the machine manufacturer:",
+                            value = 2, min = 1, max = 999),
                actionButton("predictButtonID", "Predict!"))
     ),
     fluidRow(
